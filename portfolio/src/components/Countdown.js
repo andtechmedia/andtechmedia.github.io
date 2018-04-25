4f4ded32481ee117b061ec8f0930d1431f932b3a
Eko Andri <ekoandri@MacBook-Pro-2.local>
2018-04-25 10:59:32 +0700

    hero from medium to large

--- portfolio/src/components/Countdown.js
+++ portfolio/src/components/Countdown.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Countdown = props => (
-  <section className="hero is-transparent is-medium">
+  <section className="hero is-transparent is-large">
     <div className="hero-body has-text-centered">
       <div className="container">
-        <h1 className="title">Eko Andri Subarnanto</h1>
-        <h2 className="subtitle">Lebaran is coming!</h2>
+        <h1 className="title">Lebaran is coming!</h1>
+        {/* <h2 className="subtitle">Lebaran is coming!</h2> */}
         <h2 className="subtitle">Bentar lagi Idul Fitri!</h2>
       </div>
     </div>
